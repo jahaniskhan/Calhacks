75042abacfd98223fb34276a63cd2936346ee974
Shervinsasl <136936840+Shervinsasl@users.noreply.github.com>
2023-06-17 17:33:48 -0700

    Update vectorize.py

--- vectorize.py
+++ vectorize.py
@@ -1,5 +1,6 @@
 from sentence_transformers import SentenceTransformer
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
 model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
 
@@ -10,6 +11,13 @@ sentences = df['Description'].values
 
 embeddings  = model.encode(sentences)
 print(df.columns)
+print(embeddings)
+query = "quenching bath from oilbath with syanides"
+query_embedding = model.encode([query])[0]
+similarities = cosine_similarity([query_embedding], embeddings)
+most_similar_index = similarities.argmax()
+print(sentences[most_similar_index])
+
 
 
 
